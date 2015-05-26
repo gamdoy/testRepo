eb87c935c94061195c88c72011dcb6994e118d9a
Computer <Computer@Computer-HP>
2015-05-26 14:58:57 +0900

    변동 테스트

--- MemberManager_springmvc/src/com/mycom/common/interceptor/LoginCheckInterceptor.java
+++ MemberManager_springmvc/src/com/mycom/common/interceptor/LoginCheckInterceptor.java
@@ -25,7 +25,7 @@ public class LoginCheckInterceptor extends HandlerInterceptorAdapter {
 			HttpServletResponse response, Object handler,
 			ModelAndView modelAndView) throws Exception {
 		//로그아웃 후 웹 브라우저 back 버튼 클릭시 이전페이지에 로그인 된 상태로 보이는 것 안되도록 처리
-		//로그인한 후 보는 모든 페이지들이 cache 사용하지 않도록 처리한다..
+		//로그인한 후 보는 모든 페이지들이 cache 사용하지 않도록 처리한다.
 		
 		response.setHeader("cache-control", "no-store, no-cache");
 	}
